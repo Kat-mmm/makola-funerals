6cada63e96a7bb1269158b7ecc06e0894dfa2ba3
Katlego Mothibe <Kaymothibe3@gmail.com>
2024-05-15 19:26:46 +0200

    requested changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
-function toggleNav() {
-    document.querySelector('.nav-links').classList.toggle('active');
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const accordionHeaders = document.querySelectorAll(".accordion-header");
 
+    accordionHeaders.forEach(header => {
+        header.addEventListener("click", function() {
+            const accordionItem = this.parentNode;
+            const accordionContent = accordionItem.querySelector(".accordion-content");
+            const icon = this.querySelector("span i");
+
+            if (accordionContent.style.display === "block") {
+                accordionContent.style.display = "none";
+                icon.classList.remove("fa-minus");
+                icon.classList.add("fa-plus");
+            } else {
+                accordionContent.style.display = "block";
+                icon.classList.remove("fa-plus");
+                icon.classList.add("fa-minus");
+            }
+        });
+    });
+});
